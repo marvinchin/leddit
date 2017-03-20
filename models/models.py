220af6f6ed9f546e956004f3236214d85007298e
marvinchin <chinkunsong.marvin@gmail.com>
2017-03-21 00:37:38 +0800

    Added thread manager with new thread and upvote functionality

--- models/models.py
+++ models/models.py
@@ -8,3 +8,117 @@ class Thread:
         self.id = id
         self.topic = topic
         self.score = score
+
+"""
+ThreadManager class stores threads and handles their ordering
+Threads are stored in a list indexed by thread id
+Ordering by rank is backed by a linked list data structure
+"""
+class ThreadManager:
+    def __init__(self):
+        self.threads = []   # init threads list
+        self.nodes = {}     # init nodes dict mapping thread to rank node
+        self.head = None    # init head of ranking linked list
+        self.tail = None    # init tail of ranking linked list
+        self.insertion = None   # init insertion point for new threads,
+                                # should be the last node with least positive score
+
+    def newThread(self, topic):
+        thread = Thread(len(self.threads), topic, 0)   # create the thread object
+        self.threads.append(thread) # add it to threads list
+        node = RankNode(thread.id, None, None)  # create generic RankNode
+        self.nodes[thread.id] = node
+        if self.head is None:
+            # no other nodes in the list
+            self.head = node
+            self.tail = node
+            self.insertion = node
+        elif self.insertion is None:
+            # all other nodes have negative score
+            node.next = self.head
+            self.head = node
+            self.insertion = node
+        else:
+            # there are other nodes, insert behind node at insertion point
+            self.insertion.insertAfter(node)
+            if (self.insertion == self.tail):
+                # insertion point is at the end of the list,
+                # update tail to inserted node
+                self.tail = node
+            self.insertion = node
+
+    def upvoteThread(self, threadId):
+        thread = self.threads[threadId]
+        thread.score += 1
+        # update the linked list
+        node = self.nodes[threadId]
+        # check if node is the insertion point. Update if it is
+        if self.insertion == node and node.prevNode is not None:
+            self.insertion = node.prevNode
+
+        if node == self.head:
+            # already front of the list, no movement
+            return
+        else:
+            considerNode = node.prevNode
+            node.remove() # remove node from it's position from linked list
+            considerThread = self.threads[considerNode.threadId]
+            while considerThread.score < thread.score:
+                # final position of node is somewhere before this node
+                if considerNode == self.head:
+                    # at front of list, set node to head
+                    node.nextNode = considerNode
+                    considerNode.prevNode = node
+                    self.head = node
+                    # check if head is the insertion point. Update if necessary
+                    if (self.insertion == considerNode and thread.score >= 0):
+                        self.insertion = node
+                    return
+                else:
+                    # repeat consideration with next previous node
+                    considerNode = considerNode.prevNode
+                    considerThread = self.threads[considerNode.threadId]
+
+            # considerNode is the first node to the back of node's original
+            # position with score higher than node. Final position is after this
+            considerNode.insertAfter(node)
+            # check if considerNode is insertion point. Update if necessary
+            if (self.insertion == considerNode and thread.score >= 0):
+                self.insertion = node
+
+
+"""
+RankNode class provides linked list functionality to back up the ordering of
+threads by score
+"""
+class RankNode:
+    def __init__(self, threadId, prevNode, nextNode):
+        self.threadId = threadId
+        self.prevNode = prevNode
+        self.nextNode = nextNode
+
+    def insertAfter(self, newNode):
+        nextNode = self.nextNode
+        newNode.prevNode = self
+        self.nextNode = newNode
+        newNode.nextNode = nextNode
+        if nextNode is not None:
+            nextNode.prevNode = newNode
+
+    def remove(self):
+        nextNode = self.nextNode
+        prevNode = self.prevNode
+        if nextNode is not None:
+            nextNode.prevNode = prevNode
+        if prevNode is not None:
+            prevNode.nextNode = nextNode
+
+    def toList(self):
+        nodeList = []
+        nodeList.append(self.threadId)
+        if self.nextNode is None:
+            return nodeList
+        else:
+            nextList = self.nextNode.toList()
+            nodeList.extend(nextList)
+            return nodeList
--- models/models_test.py
+++ models/models_test.py
@@ -13,6 +13,48 @@ class ThreadTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             thread = models.Thread(0, topic, 0)
 
+class ThreadManagerTest(unittest.TestCase):
+    def testCreate(self):
+        manager = models.ThreadManager()
+        self.assertEqual(manager.threads, [])
+        self.assertEqual(manager.nodes, {})
+        self.assertEqual(manager.head, None)
+        self.assertEqual(manager.tail, None)
+        self.assertEqual(manager.insertion, None)
+
+    def testNewThread(self):
+        manager = models.ThreadManager()
+        manager.newThread("Hello")
+        self.assertEqual(manager.threads[0].topic, "Hello")
+        self.assertEqual(manager.nodes[0].threadId, 0)
+        self.assertEqual(manager.head.threadId, 0)
+        self.assertEqual(manager.tail.threadId, 0)
+        self.assertEqual(manager.insertion.threadId, 0)
+        manager.newThread("World")
+        self.assertEqual(manager.threads[1].topic, "World")
+        self.assertEqual(manager.nodes[1].threadId, 1)
+        self.assertEqual(manager.head.threadId, 0)
+        self.assertEqual(manager.tail.threadId, 1)
+        self.assertEqual(manager.insertion.threadId, 1)
+
+    def testUpvote(self):
+        manager = models.ThreadManager()
+        manager.newThread("Three")  #id 0
+        manager.newThread("Little") #id 1
+        manager.newThread("Pigs")   #id 2
+        manager.upvoteThread(2)
+        self.assertEqual(manager.head.toList(), [2, 0, 1])
+        manager.upvoteThread(2)
+        self.assertEqual(manager.head.toList(), [2, 0, 1])
+        manager.newThread("Oink")   #id 3
+        self.assertEqual(manager.head.toList(), [2, 0, 1, 3])
+        manager.upvoteThread(3)
+        self.assertEqual(manager.head.toList(), [2, 3, 0, 1])
+        manager.upvoteThread(3)
+        self.assertEqual(manager.head.toList(), [2, 3, 0, 1])
+        manager.upvoteThread(3)
+        self.assertEqual(manager.head.toList(), [3, 2, 0, 1])
+        self.assertEqual(manager.threads[3].score, 3)
 
 if __name__ == "__main__":
     unittest.main();
