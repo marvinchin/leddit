5c3f3db75aaaa4fe1a0bcc427aab2069ce813921
marvinchin <chinkunsong.marvin@gmail.com>
2017-03-22 09:46:16 +0800

    added upvote and downvote functionality to homepage

--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,28 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from . import home
 from .. import threadManager
 
+"""
+Homepage that displays top 20 threads
+"""
 @home.route('/')
 def homepage():
     threads = threadManager.getThreads(0, 20)
     return render_template('home/index.html', title="Title", threads=threads)
+
+"""
+Upvote functionality for homepage, updates and redirects back to homepage
+"""
+@home.route('/upvote/<int:threadId>')
+def upvote_thread(threadId):
+    threadManager.upvoteThread(threadId)
+    return redirect(url_for('home.homepage'))
+
+"""
+Downvote functionality for homepage, updates and redirects back to homepage
+"""
+@home.route('/downvote/<int:threadId>')
+def downvote_thread(threadId):
+    threadManager.downvoteThread(threadId)
+    return redirect(url_for('home.homepage'))
--- app/thread/views.py
+++ app/thread/views.py
@@ -3,6 +3,10 @@ from forms import ThreadForm
 from .. import threadManager
 from . import thread
 
+"""
+Page for creation of new thread, redirects to homepage upon success, or backed
+to page with error message if failure
+"""
 @thread.route('/new', methods=['GET', 'POST'])
 def add_thread():
     form = ThreadForm()
@@ -15,17 +19,26 @@ def add_thread():
         return redirect(url_for('home.homepage'))
     return render_template('thread/add.html', title = "New Thread", form = form)
 
+"""
+Page to view thread with given id
+"""
 @thread.route('/<int:threadId>')
 def view_thread(threadId):
     thread = threadManager.getThread(threadId)
     title = "Thread {0}".format(threadId)
     return render_template('thread/thread.html', title = title, thread = thread)
 
+"""
+Upvote functionality for thread page, redirects to thread page
+"""
 @thread.route('/upvote/<int:threadId>')
 def upvote_thread(threadId):
     threadManager.upvoteThread(threadId)
     return redirect(url_for('thread.view_thread', threadId = threadId))
 
+"""
+Downvote functionality for thread page, redirects to thread page
+"""
 @thread.route('/downvote/<int:threadId>')
 def downvote_thread(threadId):
     threadManager.downvoteThread(threadId)
