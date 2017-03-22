777ba4633363c4e13d2ecce807b76911685eecb1
marvinchin <chinkunsong.marvin@gmail.com>
2017-03-22 11:49:24 +0800

    updated app to read port from environment

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 
 from app import create_app
 
-app = create_app()
+port = int(os.getenv('PORT', 5000))
 
-if __name__ == '__main__':
-    app.run()
+app = create_app()
+app.run(host = '0.0.0.0', port = port)
